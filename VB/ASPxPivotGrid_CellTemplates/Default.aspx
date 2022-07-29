06784b24ff2807e3309112b8f1b73b64ba2d0cd9
DevExpressExampleBot <fakeEmail>
2022-07-29 12:56:36 +0400

    Source auto update [skip ci]

--- VB/ASPxPivotGrid_CellTemplates/Default.aspx
+++ VB/ASPxPivotGrid_CellTemplates/Default.aspx
@@ -1,49 +1,49 @@
-﻿<%@ Page Language="vb" AutoEventWireup="true" CodeBehind="Default.aspx.vb"
-			Inherits="CellTemplates._Default" %>
+<%@ Page Language="VB" AutoEventWireup="true" CodeBehind="Default.aspx.vb"
+            Inherits="CellTemplates._Default" %>
 
 <%@ Register Assembly="DevExpress.Web.ASPxPivotGrid.v13.1, Version=13.1.14.0,
-			Culture=neutral, PublicKeyToken=b88d1754d700e49a"
-			Namespace="DevExpress.Web.ASPxPivotGrid"
-			TagPrefix="dx" %>
+            Culture=neutral, PublicKeyToken=b88d1754d700e49a"
+            Namespace="DevExpress.Web.ASPxPivotGrid"
+            TagPrefix="dx" %>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-		  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+          "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 
 <html xmlns="http://www.w3.org/1999/xhtml" >
 <head runat="server">
-	<title></title>
+    <title></title>
 </head>
 <body>
-	<form id="form1" runat="server">
-	<div>
-		<dx:ASPxPivotGrid ID="ASPxPivotGrid1" runat="server" 
-			DataSourceID="AccessDataSource1">
-			<Fields>
-				<dx:PivotGridField ID="fieldCountry" Area="ColumnArea" AreaIndex="0" 
-					FieldName="Country">
-				</dx:PivotGridField>
-				<dx:PivotGridField ID="fieldUnitPrice" Area="DataArea" AreaIndex="0" 
-					FieldName="UnitPrice">
-				</dx:PivotGridField>
-				<dx:PivotGridField ID="fieldQuantity" Area="DataArea" AreaIndex="1" 
-					FieldName="Quantity">
-				</dx:PivotGridField>
-				<dx:PivotGridField ID="fieldSalesperson" Area="RowArea" AreaIndex="0" 
-					FieldName="Salesperson">
-				</dx:PivotGridField>
-				<dx:PivotGridField ID="fieldPercents" Area="DataArea" AreaIndex="2"
-					  Caption="Percents" FieldName="Quantity"
-					  SummaryDisplayType="PercentOfColumn">
-				</dx:PivotGridField>
-			</Fields>
-		</dx:ASPxPivotGrid>
-		<asp:AccessDataSource ID="AccessDataSource1" runat="server" 
-			DataFile="~/App_Data/nwind.mdb" 
-			SelectCommand="SELECT [City], [Region], [Country],
-					[UnitPrice], [Quantity], [Salesperson]
-					FROM [Invoices]">
-		</asp:AccessDataSource>
-	</div>
-	</form>
+    <form id="form1" runat="server">
+    <div>
+        <dx:ASPxPivotGrid ID="ASPxPivotGrid1" runat="server" 
+            DataSourceID="AccessDataSource1">
+            <Fields>
+                <dx:PivotGridField ID="fieldCountry" Area="ColumnArea" AreaIndex="0" 
+                    FieldName="Country">
+                </dx:PivotGridField>
+                <dx:PivotGridField ID="fieldUnitPrice" Area="DataArea" AreaIndex="0" 
+                    FieldName="UnitPrice">
+                </dx:PivotGridField>
+                <dx:PivotGridField ID="fieldQuantity" Area="DataArea" AreaIndex="1" 
+                    FieldName="Quantity">
+                </dx:PivotGridField>
+                <dx:PivotGridField ID="fieldSalesperson" Area="RowArea" AreaIndex="0" 
+                    FieldName="Salesperson">
+                </dx:PivotGridField>
+                <dx:PivotGridField ID="fieldPercents" Area="DataArea" AreaIndex="2"
+                      Caption="Percents" FieldName="Quantity"
+                      SummaryDisplayType="PercentOfColumn">
+                </dx:PivotGridField>
+            </Fields>
+        </dx:ASPxPivotGrid>
+        <asp:AccessDataSource ID="AccessDataSource1" runat="server" 
+            DataFile="~/App_Data/nwind.mdb" 
+            SelectCommand="SELECT [City], [Region], [Country],
+                    [UnitPrice], [Quantity], [Salesperson]
+                    FROM [Invoices]">
+        </asp:AccessDataSource>
+    </div>
+    </form>
 </body>
 </html>
--- VB/ASPxPivotGrid_CellTemplates/Default.aspx.vb
+++ VB/ASPxPivotGrid_CellTemplates/Default.aspx.vb
@@ -1,4 +1,3 @@
-﻿Imports Microsoft.VisualBasic
 Imports System
 Imports System.Drawing
 Imports System.Web.UI
@@ -7,49 +6,47 @@ Imports DevExpress.Web.ASPxPivotGrid
 Imports DevExpress.XtraPivotGrid
 
 Namespace CellTemplates
-	Partial Public Class _Default
-		Inherits System.Web.UI.Page
-		Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
-			ASPxPivotGrid1.CellTemplate = New CellTemplate()
-		End Sub
-		Private Class CellTemplate
-			Implements ITemplate
-			Private Sub InstantiateIn(ByVal container As Control) Implements ITemplate.InstantiateIn
-				Dim templateContainer As PivotGridCellTemplateContainer = CType(container, PivotGridCellTemplateContainer)
-				Dim field As PivotGridField = templateContainer.DataField
 
-				If field IsNot Nothing AndAlso field.Caption = "Percents" Then
-					Dim value As Integer = Convert.ToInt32(Convert.ToDecimal(templateContainer.Value) * 350)
+    Public Partial Class _Default
+        Inherits Page
 
-					Dim table As New Table()
-					table.Width = Unit.Pixel(400)
-					templateContainer.Controls.Add(table)
+        Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+            ASPxPivotGrid1.CellTemplate = New CellTemplate()
+        End Sub
 
-					Dim row As New TableRow()
-					table.Controls.Add(row)
+        Private Class CellTemplate
+            Implements ITemplate
 
-					Dim cell As New TableCell()
-					cell.Style.Add(HtmlTextWriterStyle.Padding, "0px")
-					cell.Width = Unit.Pixel(value)
-					cell.Height = Unit.Pixel(20)
-					If templateContainer.Item.RowValueType = PivotGridValueType.Total Then
-						cell.BackColor = Color.Blue
-					Else
-						cell.BackColor = Color.Red
-					End If
-					If templateContainer.Item.RowValueType = PivotGridValueType.GrandTotal Then
-						cell.BackColor = Color.Green
-					End If
-					row.Controls.Add(cell)
+            Private Sub InstantiateIn(ByVal container As Control) Implements ITemplate.InstantiateIn
+                Dim templateContainer As PivotGridCellTemplateContainer = CType(container, PivotGridCellTemplateContainer)
+                Dim field As PivotGridField = templateContainer.DataField
+                If field IsNot Nothing AndAlso Equals(field.Caption, "Percents") Then
+                    Dim value As Integer = Convert.ToInt32(Convert.ToDecimal(templateContainer.Value) * 350)
+                    Dim table As Table = New Table()
+                    table.Width = Unit.Pixel(400)
+                    templateContainer.Controls.Add(table)
+                    Dim row As TableRow = New TableRow()
+                    table.Controls.Add(row)
+                    Dim cell As TableCell = New TableCell()
+                    cell.Style.Add(HtmlTextWriterStyle.Padding, "0px")
+                    cell.Width = Unit.Pixel(value)
+                    cell.Height = Unit.Pixel(20)
+                    If templateContainer.Item.RowValueType = PivotGridValueType.Total Then
+                        cell.BackColor = Color.Blue
+                    Else
+                        cell.BackColor = Color.Red
+                    End If
 
-					cell = New TableCell()
-					cell.Text = templateContainer.Item.Text
-					cell.Wrap = False
-					row.Controls.Add(cell)
-				Else
-					templateContainer.Controls.Add(New LiteralControl(templateContainer.Text))
-				End If
-			End Sub
-		End Class
-	End Class
+                    If templateContainer.Item.RowValueType = PivotGridValueType.GrandTotal Then cell.BackColor = Color.Green
+                    row.Controls.Add(cell)
+                    cell = New TableCell()
+                    cell.Text = templateContainer.Item.Text
+                    cell.Wrap = False
+                    row.Controls.Add(cell)
+                Else
+                    templateContainer.Controls.Add(New LiteralControl(templateContainer.Text))
+                End If
+            End Sub
+        End Class
+    End Class
 End Namespace
